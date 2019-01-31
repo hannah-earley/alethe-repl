199c707c3ccecb36cdc3cc9a901899529f70b245
William Earley <william@earley.org.uk>
2019-01-31 17:24:07 +0000

    cleared out warnings, added pretty printing

--- src/Language.hs
+++ src/Language.hs
@@ -4,37 +4,80 @@ import Data.Set (Set)
 import qualified Data.Set as S
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
+import Data.Vector (Vector,(!))
+import Data.Char
+import Data.Maybe (catMaybes)
+import Data.List (intercalate)
 import Control.Monad (liftM2)
 import qualified Text.Parsec.Error as PE
+
+import Helper
+
+-- main interface
 
 data Term = Atom     Integer String
           | Var      String
           | Asymm    Term Term
           | Compound [Term]
-          deriving (Eq,Show)
+          deriving (Eq)
 
 data Definition = Terminus [Term]
-                | Rule { lhs   :: [Context]
-                       , rhs   :: [Context]
-                       , decls :: [Declaration] }
-                deriving (Show)
+                | Rule { _dLPatt :: [Context]
+                       , _dRPatt :: [Context]
+                       , _dRules :: [Declaration] }
 
 data Declaration = Declaration { weight :: Int , rule :: Context}
-                deriving (Show)
 
 data Context = Context Term [Term]
-             deriving (Show)
 
-data KappaError = ParseError PE.ParseError
-                | AmbiguityError [Definition]
-                | CompilationError [Definition]
-                -- | IrreversibilityError [Definition]
-                -- ...
-                deriving (Show)
+-- display, sugar, properties, etc
 
+instance Show Term where
+    show (Atom n a)   = showAtom n a
+    show (Var v)      = v
+    show (Asymm l r)  = "(" ++ show l ++ " ! " ++ show r ++ ")"
+    show (Compound t) = showComp t
+
+instance Show Definition where
+    show (Terminus t)         = "! " ++ showSp t ++ ";"
+    show (Rule lhs rhs rules) = showCtxts lhs ++ " = " ++ showCtxts rhs
+                                              ++ showRules show rules
+
+showCtxts [c] = show c
+showCtxts cs = "{ " ++ showSemi cs ++ " }"
+
+showRules _ [] = ";"
+showRules f xs = ":" ++ concatMap (("\n    " ++) . f) xs
+
+showAtom' a = if invalidAtom a then "#" ++ show a else a
+showAtom n a | at == atomZero = "0"
+             | at == atomNil  = "[]"
+             where at = Atom n a
+showAtom 0 a = showAtom' a
+showAtom n a = "~" ++ show n ++ ":" ++ showAtom' a
+
+showComp t = let c = Compound t in
+             case catMaybes [show <$> nat' c, show <$> str' c, sexpr' c] of
+               (x:_) -> x
+               []    -> "(" ++ showSp t ++ ")"
+
+instance Show Declaration where
+    show (Declaration w r) = show r ++ replicate w '.'
+
+instance Show Context where
+    show (Context c p) = show c ++ "| " ++ showSp p
+
+nonVisible       = isControl        ||| isSpace  ||| isSeparator
+reservedIdLetter = nonVisible       ||| (`elem` ".:;`#|!$=~@()[]{}\"")
+reservedIdStart  = reservedIdLetter ||| isDigit
+reservedOpStart  = reservedIdStart  ||| isLetter ||| (`elem` "'")
+
+invalidAtom (x:xs) = isLower x || reservedIdStart x || any reservedIdLetter xs
+invalidAtom [] = False
+
+atom = Atom 0
 term0 = Compound []
 termTerm = Compound []
-atom = Atom 0
 phony = Atom (-1) ""
 atomZero = atom "Z"
 atomSucc = atom "S"
@@ -42,7 +85,7 @@ atomNil = atom "Nil"
 atomCons = atom "Cons"
 atomPlus = atom "Plus"
 atomMinus = atom "Minus"
-atomChar c = atom [c]
+atomChar c = atom ['\'', c]
 
 nats = iterate (\n -> Compound [atomSucc, n]) atomZero
 nat = (nats!!) . fromIntegral
@@ -51,7 +94,21 @@ sexpr = flip $ foldr cons
 slist = foldr cons atomNil
 str = slist . map atomChar
 
---
+nat' (Compound [x,y]) | x == atomSucc = (1+) <$> nat' y
+nat' a                | a == atomZero = Just (0 :: Integer)
+nat' _                                = Nothing
+
+str' (Compound [x, Atom 0 ['\'', c], cs]) | x == atomCons = (c:) <$> str' cs
+str' a                                    | a == atomNil  = Just ""
+str' _                                                    = Nothing
+
+sexpr' t@(Compound [x, _, _]) | x == atomCons = Just $ "[" ++ intercalate " " (sexpr'' t) ++ "]"
+sexpr' _                                      = Nothing
+sexpr'' (Compound [x, car, cdr]) | x == atomCons = show car : sexpr'' cdr
+sexpr'' a                        | a == atomNil  = []
+sexpr'' e                                        = [".", show e]
+
+-- term manipulation
 
 termLeft :: Term -> Term
 termLeft (Asymm l _) = l
@@ -115,3 +172,27 @@ instance Kappa Context where
     vars (Context c p) = S.union (vars c) (vars p)
     unify (Context c1 p1) (Context c2 p2) = liftM2 (M.union) (unify p1 p2) (unify c1 c2)
     compatible (Context c1 p1) (Context c2 p2) = compatible c1 c2 && compatible p1 p2
+
+-- evaluation
+
+newtype Program = Program [Strategy]
+
+data Strategy = StratHalt [Term]
+              | Strategy { _stLPatt :: [Context]
+                         , _stRules :: Vector Context
+                         , _stPlan  :: [Either Int Int]
+                         , _stRPatt :: [Context] }
+
+data KappaError = ParseError PE.ParseError
+                | AmbiguityError [Definition]
+                | CompilationError [Definition]
+                deriving (Show)
+
+instance Show Strategy where
+    show (StratHalt t) = show (Terminus t)
+    show (Strategy l d p r) = showCtxts l ++ " = " ++ showCtxts r ++ showRules showp p
+      where showp (Left  n) = "< " ++ show (d ! n) ++ "."
+            showp (Right n) = "> " ++ show (d ! n) ++ "."
+
+instance Show Program where
+    show (Program xs) = showMany "\n" xs
